daee1171746dc0ae0c9dbfacd8086fc0c0c21db3
AYUSH-1304 <tusharayush2003@gmail.com>
2026-07-18 11:24:54 +0530

    dictornary, set, and its method and operation

---
+++ chapter5/problem1.py
@@ -0,0 +1,7 @@
+words = {
+    "madad": "help",
+    "kursi":"Chair",
+    "billi":"Cat"
+}
+word = input("Enter the word you want meaning meaning of :")
+print(words[word]) 
---
+++ chapter5/problem9.py
@@ -0,0 +1,4 @@
+s = {8, 7, 12, "Harry", [1,2]}
+
+#s[4][0] = 9 #can not change index in set
+print(s)
